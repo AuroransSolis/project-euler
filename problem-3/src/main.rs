779cec4cbdeed9f08b11e63026b507b6d3c999f7
Aurorans Solis <primalucegd@gmail.com>
2020-07-09 17:24:22 -0700

    Did some optimizing. Now runs in about 0.04s on my desktop.

--- problem-3/src/main.rs
+++ problem-3/src/main.rs
@@ -4,11 +4,19 @@ fn main() {
     let mut num = CHECK;
     let mut primes = vec![2];
     let mut largest = 0;
-    for candidate in (1..=(CHECK as f64).sqrt() as u64 / 2).map(|n| n * 2 + 1) {
-        if !primes.iter().any(|&prime| candidate % prime == 0) {
+    for candidate in (1..).map(|n| n * 2 + 1) {
+        if candidate > num {
+            break;
+        } else if !primes
+            .iter()
+            .take_while(|&prime| prime * prime < candidate)
+            .any(|&prime| candidate % prime == 0)
+        {
             primes.push(candidate);
             if num % candidate == 0 {
-                num /= candidate;
+                while num % candidate == 0 {
+                    num /= candidate;
+                }
                 largest = candidate;
             }
         }
