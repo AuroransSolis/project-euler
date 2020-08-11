d9a7474dc75971891747bff2b463949a33033bd6
Aurorans Solis <primalucegd@gmail.com>
2020-08-11 00:19:55 -0700

    Made problem 1 way more efficient.

--- problem-1/src/bench.rs
+++ problem-1/src/bench.rs
@@ -1,31 +1,34 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-macro_rules! build_benchmark {
-    ($($max:literal),+) => {
-        fn bench_solution(c: &mut Criterion) {
-            $(
-                c.bench_function(format!("problem 1 (max: {})", $max).as_str(), |b| {
-                    b.iter(|| black_box(get_sum($max)))
-                });
-            )*
-        }
-    }
-}
-
 fn get_sum(max: usize) -> usize {
-    (0..=max / 15)
-        .map(|n| {
-        [0, 3, 5, 6, 9, 10, 12]
-            .iter()
-            .map(|&s| n * 15 + s)
-            .take_while(|&t| t < max)
-            .sum::<usize>()
+    let mul = max / 15;
+    let res_max = max % 15;
+    [0, 3, 5, 6, 9, 10, 12]
+        .iter()
+        .copied()
+        .map(|residue| {
+            if residue < res_max {
+                arithmetic_sequence_sum(residue, 15, mul + 1)
+            } else {
+                arithmetic_sequence_sum(residue, 15, mul)
+            }
         })
         .sum::<usize>()
 }
 
-build_benchmark! {
-    1000, 10_000, 100_000, 1_000_000, 10_000_000
+#[inline]
+fn arithmetic_sequence_sum(start: usize, diff: usize, n: usize) -> usize {
+    (n * (2 * start + (n - 1) * diff)) / 2
+}
+
+const MAXES: [usize; 5] = [1000, 10_000, 100_000, 1_000_000, 10_000_000];
+
+fn bench_solution(c: &mut Criterion) {
+    for max in MAXES.iter().copied() {
+        c.bench_function(format!("problem 1 (max: {})", max).as_str(), |b| {
+            b.iter(|| black_box(get_sum(max)))
+        });
+    }
 }
 
 criterion_group! {
--- problem-1/src/main.rs
+++ problem-1/src/main.rs
@@ -18,5 +18,27 @@ fn main() {
                     .sum::<usize>()
             })
             .sum::<usize>();
-    println!("p1: {}", sum);
+    println!("p1 (iter): {}", sum);
+    println!("p1 (func): {}", get_sum(MAX));
+}
+
+fn get_sum(max: usize) -> usize {
+    let mul = max / 15;
+    let res_max = max % 15;
+    [0, 3, 5, 6, 9, 10, 12]
+        .iter()
+        .copied()
+        .map(|residue| {
+            if residue < res_max {
+                arithmetic_sequence_sum(residue, 15, mul + 1)
+            } else {
+                arithmetic_sequence_sum(residue, 15, mul)
+            }
+        })
+        .sum::<usize>()
+}
+
+#[inline]
+fn arithmetic_sequence_sum(start: usize, diff: usize, n: usize) -> usize {
+    (n * (2 * start + (n - 1) * diff)) / 2
 }
